f219881c9471eaef8072422b0a3ef040181e204e
lqsky7 <catinice@outlook.com>
2024-11-02 21:42:37 +0530

    setup cors and common middleware
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 import express from "express";
-
 const app = express();
+
+import cors from "cors"; // Its purpose is to decide who should be able to access backend, for example it can be made such that only some particular frontend is able to hit endpoints. 
+
+// only allow urls mentioned in .env to hit server
+app.use(
+    cors({
+        origin: process.env.CORSVAR,
+        credentials: true
+    })
+)
+// common middleware: intercepts requests before they hit server.
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
 export {app};

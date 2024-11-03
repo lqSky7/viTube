f759b6b9dfe74a2e65856d996aadb69d70ceef41
lqsky7 <catinice@outlook.com>
2024-11-03 16:01:50 +0530

    healthcheck
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,3 +1,9 @@
+import { response } from "express";
 import { ApiResponse } from "../utils/apiResponse";
 import { asyncHandler } from "../utils/ayncHandler";
 
+const healthCheck = asyncHandler(async(request, response) => {
+    return response.status(200).json(new ApiResponse(200,"none","test ok"))
+});
+
+export { healthCheck };

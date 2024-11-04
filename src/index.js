77b3e85d47dddeecc435ddd02ff7a911eb58e27d
lqsky7 <catinice@outlook.com>
2024-11-04 12:49:38 +0530

    usermodel schema
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- src/index.js
+++ src/index.js
@@ -6,18 +6,12 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-
-
-
 const isRunning = async () => {
     await db();
         app.listen(process.env.PORT, () => {
-            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-            
+            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);    
         });
-        
-       // Not using else here since db else case will kill the process, thus never returning false.
-}
+     }
 
 
 isRunning()
---
+++ src/models/user.models.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+// note 1: _id is a unique id added to every schema automatically by mongodb.
+const UserSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+        trim: true,
+    },
+    email: {required: true,
+        unique: true,
+        index: true,
+        trim: true,
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+        trim: true,
+    },
+    fullname: {
+        type: String,
+        required: false,
+        trim: true,
+    },
+    avatar: {
+        type: String, //url
+    },
+
+    watchHistory: {
+        type: Schema.Types.ObjectId,
+        ref: "Videos",
+    },
+    password: {
+        type: String, 
+        required: [true, "password is a required field..."]
+    }, 
+    refreshToken:{
+        type: String,
+    }
+    
+}, {
+    timestamps: true // automatically generates updatedAt and createdAt stamps.
+})
+
+
+
+export const User = mongoose.model("User", UserSchema)

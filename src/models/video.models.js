d8e9b17304ddc96848135159796725e48e761a62
lqsky7 <catinice@outlook.com>
2024-11-04 13:02:13 +0530

    video model schema
    Signed-off-by: lqsky7 <catinice@outlook.com>

---
+++ src/models/video.models.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const VideoSchema = new Schema({
+    thumbnail:{
+        type: String, // link
+        required: false
+    },
+    videoFile: {
+        type: String, // link
+        required: true,
+    },
+
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    description: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    description: {
+        type: Boolean,
+        default: true,
+    },
+
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{
+    timestamps: true
+})
+
+export const Video = mongoose.model("Videos", VideoSchema)

c632389f26d42c4befdf3657330beca2f2bed39b
lqsky7 <catinice@outlook.com>
2024-11-07 23:07:34 +0530

    connect to atlas
    Signed-off-by: lqsky7 <catinice@outlook.com>

---
+++ src/db/connect.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+import dotenv from "dotenv";
+dotenv.config();
+
+ 
+const connectDB = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.atlas_url}/${DB_NAME}`);
+        console.log("connected", connectionInstance.connection.host);
+        
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+        
+    }
+}
+
+export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("w");
+import connectDB from "./db/connect";
+import dotenv from 'dotenv';
+dotenv.config({path: "./env"});
+connectDB()

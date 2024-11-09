1a24bd881b406b661bee41a9644f4709554f0e13
lqsky7 <catinice@outlook.com>
2024-11-09 10:43:42 +0530

    setup cors and cors origin env var
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import connectDB from "./db/connect";
 import dotenv from 'dotenv';
 dotenv.config({path: "./env"});
 import { app } from "./app";
+import cors from "cors";
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}))
 
 connectDB().then(() => {app.listen(process.env.port || 3001, () => {console.log("mongoconnect.then -> server is listening");
 })}).catch(error => {console.log("atlas connect catch error: ",error);

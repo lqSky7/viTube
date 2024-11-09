c538cace52e44af4090d3ed16275ee5947bee95b
lqsky7 <catinice@outlook.com>
2024-11-09 10:09:07 +0530

    start express server
    * dont forget this: npm install -D @types/express
    
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import { error } from "console";
 import connectDB from "./db/connect";
 import dotenv from 'dotenv';
 dotenv.config({path: "./env"});
-connectDB()
+import { app } from "./app";
+
+connectDB().then(() => {app.listen(process.env.port || 3001, () => {console.log("mongoconnect.then -> server is listening");
+})}).catch(error => {console.log("atlas connect catch error: ",error);
+})

10076cf3cd9612d425b231c86913136628f693af
lqsky7 <catinice@outlook.com>
2024-11-10 17:33:08 +0530

    asynchandler 🔥 🔥
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import express from "express";
 const app = express();
+import cors from "cors";
+import cookieparser from "cookie-parser";
 
+//middleware
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+}))
+app.use(express.json({limit: "16kb"})) // server accept json files
+app.use(express.static("public")) // store temp files in public dir
+app.use(express.urlencoded({ extended: true, limit: "16kb" })) // server can read from url, extened means objects within objects in url
+app.use(cookieparser()) // get cookies from browser into a more readble format
 
 export { app };

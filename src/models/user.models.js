e1a5aaa8d6844d9e313bee54fe14f3142e01576d
lqsky7 <catinice@outlook.com>
2025-01-09 21:07:05 +0530

    Add JWT token generation methods to User model
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -10,6 +10,9 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from "bcrypt"
 import mongoose, { Schema } from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config({path: "./env"});
+
 const userSchema= new mongoose.Schema({
     username:{
         type: String,
@@ -66,4 +69,18 @@ userSchema.methods.isPassCrct(async function (password) {
     return await bcrypt.compare(password, this.password);
 })
     
+userSchema.methods.genAccessTokkken(async function(){ //maybe remove async
+    await jwt.sign({
+        _id: userSchema._id,
+        email: userSchema.email,
+        username: userSchema.username
+    }, process.env.ACCESS_TOKEN_PRIV,{expiresIn: process.env.ACCESS_TOKEN_EXP})
+})
+
+userSchema.methods.genRefreshTokkken(async function(){ //maybe remove async
+    await jwt.sign({
+        _id: userSchema._id,
+    }, process.env.REF_TOKEN_PRIV,{expiresIn: process.env.REF_TOKEN_EXP})
+})
+
 export const User = mongoose.model("User", userSchema);

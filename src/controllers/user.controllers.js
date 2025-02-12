182399ce70b92443854dba35ac22072c75b35135
lqsky7 <catinice@outlook.com>
2025-02-12 22:53:58 +0530

    complete user controllers with avatar upload Signed-off-by: lqsky7 <catinice@outlook.com>

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,6 +2,8 @@ import { User } from "../models/user.models";
 import { errApi } from "../utils/errApi";
 import { log } from "node:console";
 import { asyncHandler } from "../utils/asyncHandler";
+import { uppToCloudinary } from "../utils/cloudinary";
+import { apiResponse } from "../utils/responseApi";
 // take post request from user
 // check if user already exist: email indexing
 // if yes, take avatar image input. get it through middleware and push it to cloudinary
@@ -24,8 +26,8 @@ const registerUser = asyncHandler(async(req,res) => {
     if(username == ""){
         throw errApi(400, "Username is required");
     }
-    const Userexists = User.findOne(username);
-    const mailexists = User.findOne(email);
+    const Userexists = await User.findOne(username);
+    const mailexists = await User.findOne(email);
     if(Userexists || mailexists){
         throw new errApi(409, "User/Mail Already Exist!")
     }
@@ -36,6 +38,28 @@ const registerUser = asyncHandler(async(req,res) => {
     if(!avatarLocalpath){
         throw new errApi(409,"Avatar Image not found on local sever: multer issue or user did not upload properly");
     }
+    const avt = await uppToCloudinary(avatarLocalpath);
+    const coverImg = await uppToCloudinary(coverLocalpath);
+
+    if(!avt){
+        throw new errApi(500,"Server side issue: Avatar Image not uploaded to cloudinary");
+    }
+
+    const user = await User.create(
+        {
+            Fullname,
+            email,
+            password,
+            username,
+            avatar: avt.url,
+            coverimage: coverImg?.url || ""
+        })
+    createdUser = await User.findById(user._id).select("-password -refreshToken") //in select syntax, everything is selected by default, we just removed those 2. and createdUser jab aayega usme in dono ke alawa saare fields select hokr aayenge
+    if(!createdUser){
+        throw new errApi(500, "Server side issue: Couldn't register user")
+    }
+
+    return res.status(201).json(new apiResponse(200), createdUser, "User reg successful")
 });
 
-export { registerUser };
+export { registerUser }

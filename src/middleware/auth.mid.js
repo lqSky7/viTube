ce6750b3333371b2bb0c4dc09e4befb82b495e60
lqsky7 <catinice@outlook.com>
2025-02-14 20:59:25 +0530

    JWT auth middleware to verify and extract user _id from cookies
    Signed-off-by: lqsky7 <catinice@outlook.com>

---
+++ src/middleware/auth.mid.js
@@ -0,0 +1,24 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { errApi } from "../utils/errApi";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models";
+// we verify if user has correct access refresh token
+export const verifyJWT = asyncHandler(async(req,res, next) => {
+    try {
+        const tokenFromUser = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        if (!tokenFromUser) {
+            throw new errApi(401, "no cookies passed from user");
+        }
+    
+        const deCodedToken = jwt.verify(tokenFromUser, process.env.ACCESS_TOKEN_PRIV);
+    
+        const user = await User.findById(deCodedToken?._id).select("-password -refreshToken")
+    
+        if (!user) {throw new errApi(401, "user cookie is wrong!");}
+    
+        req.user123 = user;
+        next();
+    } catch (error) {
+        throw errApi(401, error);
+    }
+})

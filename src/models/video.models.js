af0b4ea3170a7a93fac29ff4e80896307d33520a
lqsky7 <catinice@outlook.com>
2025-02-18 21:39:59 +0530

    add remaining models ⊧
    Signed-off-by: lqsky7 <catinice@outlook.com>

---
+++ src/models/video.models.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    duration: {
+      type: Number, //we fetch time from where we're uploading our video
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video", videoschema);

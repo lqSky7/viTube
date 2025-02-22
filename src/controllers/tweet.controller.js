c82df9aa67d60b2c79e65b99a4b8a901cfca93a3
lqsky7 <catinice@outlook.com>
2025-02-22 14:59:31 +0530

    (route+controller) update and delete tweet
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -4,31 +4,73 @@ import { errApi } from "../utils/errApi";
 import { asyncHandler } from "../utils/asyncHandler";
 import { apiResponse } from "../utils/responseApi";
 
-const createTweet = asyncHandler(async(req, res) => {
-    const owner = req.authorizedUser._id;
-    const username = req.authorizedUser.username;
-    const {content} = req.body;
-    if(content == undefined){
-        throw new errApi(401, "Tweet content not provided")
-    }
+const createTweet = asyncHandler(async (req, res) => {
+  const owner = req.authorizedUser._id;
+  const username = req.authorizedUser.username;
+  const { content } = req.body;
+  if (content == undefined) {
+    throw new errApi(401, "Tweet content not provided");
+  }
 
-    try {
-        const tweet = await Tweet.create({content, owner, username});
-        return res.status(200).json(new apiResponse(201, tweet, true, "Tweet created successfully"))
-    } catch (error) {
-        throw new Error(error)
-    }
+  try {
+    const tweet = await Tweet.create({ content, owner, username });
+    return res
+      .status(200)
+      .json(new apiResponse(201, tweet, true, "Tweet created successfully"));
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-})
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  try {
+    const tweet = await Tweet.find({ username });
+    return res
+      .status(200)
+      .json(new apiResponse(201, { tweet }, true, "user tweets fetched"));
+  } catch (error) {
+    throw new errApi(
+      401,
+      "Given user does not have any tweets" + error,
+      error,
+      ""
+    );
+  }
+});
 
-const getUserTweets = asyncHandler(async(req,res) => {
-    const {username} = req.params;
-    try {
-        const tweet = await Tweet.find({username})
-        return res.status(200).json(new apiResponse(201, {tweet}, true, "user tweets fetched"))
-    } catch (error) {
-        throw new errApi(401, "Given user does not have any tweets" + error, error, "")
-    }
-})
+const updateTweet = asyncHandler(async (req, res) => {
+  const { content, tweetId } = req.body;
+  if (content == undefined || tweetId == undefined) {
+    throw new errApi(401, "Tweet content not provided");
+  }
+  try {
+    const tweet = await Tweet.findByIdAndUpdate(
+      tweetId,
+      { content },
+      { new: true }
+    );
+    return res
+      .status(200)
+      .json(new apiResponse(201, tweet, true, "Tweet updated successfully"));
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
-export { createTweet , getUserTweets }
+const deleteTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.body;
+  if (tweetId == undefined) {
+    throw new errApi(401, "Tweet content not provided");
+  }
+  try {
+    const tweet = await Tweet.findByIdAndDelete(tweetId);
+    return res
+      .status(200)
+      .json(new apiResponse(201, tweet, true, "Tweet deleted successfully"));
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTweet, getUserTweets } from "../controllers/tweet.controller";
+import { createTweet, deleteTweet, getUserTweets, updateTweet } from "../controllers/tweet.controller";
 import { upload } from '../middleware/multer.mid';
 import { verifyJWT } from "../middleware/auth.mid";
 const tweetRouter = Router();
@@ -7,5 +7,6 @@ const tweetRouter = Router();
 tweetRouter.route("/get/:username").get(getUserTweets);
 // (Secured Routes 🔐)
 tweetRouter.route("/create").post(verifyJWT, createTweet);
-
+tweetRouter.route("/update").post(verifyJWT, updateTweet);
+tweetRouter.route("/delete").delete(verifyJWT, deleteTweet);
 export default tweetRouter;
